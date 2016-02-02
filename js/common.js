bad5dbd051bc1602a6117bf0aeda22db511b9988
MogilevichDmitry <dmsmain@gmail.com>
2016-02-02 14:30:19 +0300

    v0.8.7

---
+++ js/common.js
@@ -0,0 +1,126 @@
+
+common = (function($) {
+    var o = {};
+    // Final version
+    o.showTopNavigation = function(){
+        var $wrapper = $('.hidden-block-wrapper');
+        var $button = $wrapper.find('> .show-block');
+
+        $button.click(function(){
+            var $currentBlock = $(this).find('+ .hidden-block');
+
+            if ($currentBlock.hasClass('visible')) {
+                panelToggle($currentBlock); // Current block is hiding
+            } else {
+                var $activeBlock = $wrapper.find('.visible');
+                var isActiveBlock = $activeBlock.size() > 0;
+
+                if (isActiveBlock) {
+                    panelToggle($activeBlock); // Active block is hiding
+                }
+
+                panelToggle($currentBlock); // Current block is appearing
+            }
+        });
+
+        function panelToggle(panel){
+            var button = panel.parent().find('> .show-block');
+
+            panel.slideToggle(function(){
+                panel.toggleClass('visible').removeAttr('style');
+            });
+            button.toggleClass('active');
+        }
+    };
+
+
+
+    // I
+    o.sliderInitialization = function() {
+        var queryMobile = Modernizr.mq('(max-width: 767px)');
+        var queryTablet = Modernizr.mq('(min-width:768px)');
+        var queryDesktop = Modernizr.mq('(min-width: 1024px)');
+        if (queryMobile) {
+            if(!$('.bx-wrapper').size()) {
+                $('#slider1').bxSlider({
+                    mode: 'horizontal',
+                    captions: true
+                });
+
+                $('#slider2').bxSlider({
+                    mode: 'horizontal',
+                    captions: true,
+                    minSlides: 1,
+                    maxSlides: 1
+                });
+            }
+        }
+        if(queryTablet){
+            $('#slider2').bxSlider({
+                mode: 'horizontal',
+                captions: true,
+                slideWidth:210,
+                slideMargin: 20,
+                minSlides: 3,
+                maxSlides: 3
+            });
+        }
+        if(queryDesktop){
+            $('#slider3').bxSlider({
+                captions: true,
+                slideWidth:210,
+                minSlides: 5,
+                maxSlides: 5,
+                slideMargin: 192,
+                moveSlides: 1
+            });
+        }
+    };
+
+    /* FIX HEADER */
+    o.fixHeader = function() {
+        var query = Modernizr.mq('(max-width: 1024px)');
+        if (query) { //for mobile
+            $(window).scroll(function () {
+                var temp = $(".b_fix");
+                var offset = temp.offset();
+                if (offset.top <= 10) {
+                    $(".page-header").removeClass('fix-navigation-mobile');
+                }
+                else {
+                    $(".page-header").addClass('fix-navigation-mobile');
+                }
+            });
+        }
+       /* else{  // for desktop
+            $(window).scroll(function () {
+                var temp = $(".b_fix");
+                var offset = temp.offset();
+                if (offset.top <= 60) {
+                    $(".main-navigation").removeClass('fix-navigation-desktop');
+                }
+                else {
+                    $(".main-navigation").addClass('fix-navigation-desktop');
+                }
+            });
+        }*/
+    };
+
+
+    return o;
+})($);
+
+
+
+$(document).ready(function(){
+    common.showTopNavigation();
+    common.sliderInitialization();
+    common.fixHeader();
+});
+
+
+$(window).resize(function(){
+    common.sliderInitialization();
+    common.fixHeader();
+});
+
---
+++ js/event.js
@@ -0,0 +1,161 @@
+/**
+ * Created by Дмитрий on 24.01.2016.
+ */
+
+$(document).on('ready', function () {
+
+    // set visible authorization-form if click on "login"
+    $('.login-link')
+        .on('click', function(event){
+        $('.authorization-form').addClass("visible");
+            $("html,body").css("overflow","hidden");
+    });
+
+    // delete visible authorization-form if click on "close-button"
+    $('.close-auth-form')
+        .on('click', function (event) {
+            $('.authorization-form').removeClass("visible");
+            $("html,body").css("overflow","auto");
+    });
+
+    //switch jquery-tabs/angular-tabs
+    $('.checkbox')
+        .on('click', function (event){
+            $('.switcher').toggleClass("shareJquery");
+
+            if($('.switcher').hasClass("shareJquery")){
+                $('.tab-content').hide();
+                $('.partners-tabs').hide();
+                $('.jquery-tabs').show();
+            }
+            else{
+                $('.tab-content').show();
+                $('.partners-tabs').show();
+                $('.jquery-tabs').hide();
+            }
+    });
+
+    //set active class on active jquery-tab
+    $('ul.tabs-jquery').on('click', 'li:not(.jt-active)', function() {
+        $(this)
+            .addClass('jt-active').siblings().removeClass('jt-active');
+    });
+
+
+});
+
+
+function showError(container, errorMessage){
+    container.className = 'error-valid';
+    var msgElem = document.createElement('span');
+    msgElem.className = "error-message";
+    msgElem.innerHTML = errorMessage;
+    container.appendChild(msgElem);
+}
+function resetError(container) {
+    container.className = '';
+    if (container.lastChild.className == "error-message") {
+        container.removeChild(container.lastChild);
+    }
+}
+function validate(form){
+    var elems = form.elements;
+
+    resetError(elems.email.parentNode);
+    var re = /^(([a-zA-Z]|[0-9])|([-]|[_]|[.]))+[@](([a-zA-Z0-9])|([-])){2,63}[.](([a-zA-Z0-9]){2,63})+$/gi;
+
+    if (!elems.email.value) {
+        showError(elems.email.parentNode, ' Укажите email.');
+    }
+    else if (!re.test(elems.email.value)){
+        showError(elems.email.parentNode,'Проверьте правильность введенного email адреса.');
+    }
+       resetError(elems.password.parentNode);
+    if (!elems.password.value) {
+        showError(elems.password.parentNode, ' Укажите пароль.');
+    }
+}
+
+$(document).on('click', '.moreless', function() {
+
+    var tempId = "#" + $(this).attr("id");
+    var tempValue = $(tempId).val();
+    resetValue();
+
+    /* change value button*/
+    if ( tempValue == "more ▼"){
+        $(tempId).val("less ▲");
+    }
+    else {
+        $(tempId).val("more ▼");
+    }
+
+
+    /* set visible */
+    $('.more-info-about-partner').show(500);
+
+    if($(tempId).val() == "more ▼" ){
+        $('.more-info-about-partner').hide(400);
+    }
+
+    /* move array and change img src */
+    if ( tempId == "#p-ibm" || tempId == '#p-ibm-a' || tempId == '#p-ibm-b'){
+        checkExcessClasses();
+        $('.more-info-about-partner').addClass("c-ibm");
+        $('.partner-extra-img').attr("src", "./images/content/ibm-for-lb.jpg")
+    }
+    else if( tempId == "#p-hp" || tempId == '#p-hp-a' || tempId == '#p-hp-b'){
+        checkExcessClasses();
+        $('.more-info-about-partner').addClass("c-hp");
+        $('.partner-extra-img').attr("src", "./images/content/hp-for-lb.jpg")
+    }
+    else if( tempId == "#p-cisco" || tempId == '#p-cisco-a' || tempId == '#p-cisco-b'){
+        checkExcessClasses();
+        $('.more-info-about-partner').addClass("c-cisco");
+        $('.partner-extra-img').attr("src", "./images/content/cisco-for-lb.jpg")
+    }
+    else if( tempId == "#p-sas" || tempId == '#p-sas-a' || tempId == '#p-sas-b'){
+        checkExcessClasses();
+        $('.more-info-about-partner').addClass("c-sas");
+        $('.partner-extra-img').attr("src", "./images/content/sas-for-lb.jpg")
+    }
+    else if( tempId == "#p-dell" || tempId == '#p-dell-a' || tempId == '#p-dell-b'){
+        checkExcessClasses();
+        $('.more-info-about-partner').addClass("c-dell");
+        $('.partner-extra-img').attr("src", "./images/content/dell-for-lb.jpg")
+    }
+});
+
+// delete excessClasses "c-..."
+function checkExcessClasses(){
+    if( $('.more-info-about-partner').hasClass("c-ibm") || $('.more-info-about-partner').hasClass("c-hp")
+        || $('.more-info-about-partner').hasClass("c-cisco") || $('.more-info-about-partner').hasClass("c-sas")
+            ||$('.more-info-about-partner').hasClass("c-dell")){
+
+                        $('.more-info-about-partner').removeClass("c-ibm");
+                        $('.more-info-about-partner').removeClass("c-hp");
+                        $('.more-info-about-partner').removeClass("c-cisco");
+                        $('.more-info-about-partner').removeClass("c-sas");
+                        $('.more-info-about-partner').removeClass("c-dell");
+    }
+}
+
+// set all value more
+function resetValue(){
+    var more = "more ▼";
+    $('#p-ibm').val();
+    $('#p-hp').val(more);
+    $('#p-cisco').val(more);
+    $('#p-sas').val(more);
+    $('#p-dell').val(more);
+    $('#p-ibm-a').val(more);
+    $('#p-hp-a').val(more);
+    $('#p-cisco-a').val(more);
+    $('#p-sas-a').val(more);
+    $('#p-dell-a').val(more);
+    $('#p-ibm-b').val(more);
+    $('#p-hp-b').val(more);
+    $('#p-cisco-b').val(more);
+    $('#p-sas-b').val(more);
+    $('#p-dell-b').val(more);
+}
